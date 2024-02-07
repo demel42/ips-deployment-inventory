08676f8d0cb42a5dc4d37eebb318c6038243f1de
Ch. Damsky <demel42@web.de>
2024-02-07 14:40:23 +0100

    v2

--- files/smoke-detector-ack.php
+++ files/smoke-detector-ack.php
@@ -7,8 +7,8 @@ require_once IPS_GetScriptFile(GetLocalConfig('GLOBAL_HELPER'));
 $scriptName = IPS_GetName($_IPS['SELF']) . '(' . $_IPS['SELF'] . ')';
 $scriptInfo = IPS_GetName(IPS_GetParent($_IPS['SELF'])) . '\\' . IPS_GetName($_IPS['SELF']);
 
-    $catID = GetLocalConfig('Geräte-Status');
-    $varID = Variable_Create($catID, '', 'Rauchmelder ausgelöst', VARIABLETYPE_BOOLEAN, 'Local.JaNein', 0, 0);
+$catID = GetLocalConfig('Geräte-Status');
+$varID = Variable_Create($catID, '', 'Rauchmelder ausgelöst', VARIABLETYPE_BOOLEAN, 'Local.JaNein', 0, 0);
 $smokeDetected = GetValueBoolean($varID);
 
 $msg = '';
